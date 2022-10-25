d256f6c58d79aaf9f77a6658252ad3e64f2d3525
Ilona <avvakumova_ilona@mail.ru>
2022-10-25 18:31:50 +0300

    комит

--- src/main/java/com/json/gson/AppRunner.java
+++ src/main/java/com/json/gson/AppRunner.java
@@ -7,22 +7,5 @@ public class AppRunner {
        MainView menu = new MainView();
        menu.menu();
 
-     // JdbcWriterRepositoryImpl jdb = new JdbcWriterRepositoryImpl();
-     //  System.out.println(jdb.getAll());
-
-/* "Создать новый массив размера 100 и заполнить его случайными числами из диапазона от 0 до 10000.
-Затем, используя цикл for each вывести в консоль:
-наибольший элемент массива"*/
-/*int [] massiv = new int[100];
-        for (int i = 0; i < 100; i++) {
-            massiv [i]= (int) (Math.random()*10001);
-        }
-        int max =0;
-        for (int a:massiv
-             ) {
-            if(a>max)
-                max  = a;
-        }
-        System.out.println(max);*/
     }
 }
